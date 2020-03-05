3d2f2f160d47480813fd778118e2e84e79c20b6f
rileybr <brandon.riley@ssfs.org>
2020-03-05 11:21:35 -0500

    programs having to do with sorting

---
+++ src/com/company/Sorting.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Sorting {
+    public static ArrayList<Integer> getUnsorted() throws FileNotFoundException {
+        Scanner scanner = new Scanner(new File("sortedData1.txt"));
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        int count = 0;
+        while (count != 8){
+            list.add(scanner.nextInt());
+            count++;
+        }
+        return list;
+    }
+    public static boolean isSorted(ArrayList<Integer> arr){
+        for (int i = 0; i <= 8; i++)
+        return true;
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        System.out.println(getUnsorted());
+    }
+}
